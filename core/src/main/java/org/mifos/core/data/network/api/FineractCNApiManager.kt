d2b234245f3f0af17c8f80806e01a44b9f226579
Kinar Sharma <56648862+EGOR-IND@users.noreply.github.com>
2021-07-13 00:53:42 +0530

    made some changes related to tests

--- core/src/main/java/org/mifos/core/data/network/api/FineractCNApiManager.kt
+++ core/src/main/java/org/mifos/core/data/network/api/FineractCNApiManager.kt
@@ -7,30 +7,33 @@ import org.mifos.core.data.network.api.services.CustomerService
 import org.mifos.core.data.network.api.services.DepositService
 import org.mifos.core.utils.Constants
 
-class FineractCNApiManager() {
+open class FineractCNApiManager {
 
-    companion object {
-        private lateinit var httpClient: HttpClient
-        private lateinit var customerApi: CustomerService
-        private lateinit var authenticationApi: AuthenticationService
-        private lateinit var depositApi: DepositService
-        private lateinit var accountingApi: AccountingService
+    init {
+        val accessToken = ""
+        createAuthenticatedService(accessToken)
+    }
 
-        fun createAuthenticatedService(accessToken: String) {
-            httpClient = KtorClientFactory(
-                accessToken,
-                Constants.TENANT, Constants.USER
-            )
-                .build()
-            initialize()
-        }
+    protected lateinit var httpClient: HttpClient
+    private lateinit var customerApi: CustomerService
+    private lateinit var authenticationApi: AuthenticationService
+    private lateinit var depositApi: DepositService
+    private lateinit var accountingApi: AccountingService
 
-        private fun initialize() {
-            customerApi = CustomerService(httpClient)
-            authenticationApi = AuthenticationService(httpClient)
-            depositApi = DepositService(httpClient)
-            accountingApi = AccountingService(httpClient)
-        }
+    open fun createAuthenticatedService(accessToken: String) {
+        httpClient = KtorClientFactory(
+            accessToken,
+            Constants.TENANT, Constants.USER
+        )
+            .build()
+        initialize()
+    }
+
+    protected fun initialize() {
+        customerApi = CustomerService(httpClient)
+        authenticationApi = AuthenticationService(httpClient)
+        depositApi = DepositService(httpClient)
+        accountingApi = AccountingService(httpClient)
     }
 
     fun getAuthenticationAPI(): AuthenticationService = authenticationApi
