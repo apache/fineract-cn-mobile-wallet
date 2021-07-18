628c8d68ddf3bfd7d2bd032095644095ae06d86f
Kinar Sharma <56648862+EGOR-IND@users.noreply.github.com>
2021-07-19 01:14:47 +0530

    updated the core library for more ideal clean architecture

--- core/src/main/java/org/mifos/core/data/repository/WalletRepositoryImpl.kt
+++ core/src/main/java/org/mifos/core/data/repository/WalletRepositoryImpl.kt
@@ -3,23 +3,13 @@ package org.mifos.core.data.repository
 /* ktlint-disable no-wildcard-imports */
 import io.ktor.client.statement.*
 import org.mifos.core.data.network.api.FineractCNApiManager
-import org.mifos.core.data.repository.Mappers.customerEntityMapper
-import org.mifos.core.data.repository.Mappers.customerMapper
-import org.mifos.core.data.repository.Mappers.customerPageMapper
-import org.mifos.core.data.repository.Mappers.depositAccountListMapper
-import org.mifos.core.data.repository.Mappers.depositAccountMapper
-import org.mifos.core.data.repository.Mappers.depositAccountPayloadEntityMapper
-import org.mifos.core.data.repository.Mappers.journalEntryListMapper
-import org.mifos.core.data.repository.Mappers.journalEntryMapper
-import org.mifos.core.data.repository.Mappers.loginResponseMapper
-import org.mifos.core.data.repository.Mappers.productMapper
-import org.mifos.core.domain.model.LoginResponse
-import org.mifos.core.domain.model.customer.Customer
-import org.mifos.core.domain.model.customer.CustomerPage
-import org.mifos.core.domain.model.deposit.DepositAccount
-import org.mifos.core.domain.model.deposit.DepositAccountPayload
-import org.mifos.core.domain.model.deposit.Product
-import org.mifos.core.domain.model.journal.JournalEntry
+import org.mifos.core.data.network.entity.LoginResponseEntity
+import org.mifos.core.data.network.entity.customer.CustomerEntity
+import org.mifos.core.data.network.entity.customer.CustomerPageEntity
+import org.mifos.core.data.network.entity.deposit.DepositAccountEntity
+import org.mifos.core.data.network.entity.deposit.DepositAccountPayloadEntity
+import org.mifos.core.data.network.entity.deposit.ProductEntity
+import org.mifos.core.data.network.entity.journal.JournalEntryEntity
 import org.mifos.core.domain.repository.WalletRepository
 /* ktlint-enable no-wildcard-imports */
 
@@ -31,89 +21,73 @@ class WalletRepositoryImpl(private val fineractCNApiManager: FineractCNApiManage
         grantType: String,
         userName: String,
         password: String
-    ): LoginResponse {
-        return loginResponseMapper(
-            fineractCNApiManager.getAuthenticationAPI()
-                .authenticate(grantType, userName, password)
-        )
+    ): LoginResponseEntity {
+        return fineractCNApiManager.getAuthenticationAPI()
+            .authenticate(grantType, userName, password)
     }
 
-    override suspend fun createCustomer(customerPayload: Customer): HttpResponse {
+    override suspend fun createCustomer(customerPayload: CustomerEntity): HttpResponse {
         return fineractCNApiManager.getCustomerApi()
-            .createCustomer(customerEntityMapper(customerPayload))
+            .createCustomer(customerPayload)
     }
 
-    override suspend fun fetchCustomerDetails(identifier: String): Customer {
-        return customerMapper(fineractCNApiManager.getCustomerApi().fetchCustomer(identifier))
+    override suspend fun fetchCustomerDetails(identifier: String): CustomerEntity {
+        return fineractCNApiManager.getCustomerApi().fetchCustomer(identifier)
     }
 
     override suspend fun fetchCustomers(
         pageIndex: Int?,
         size: Int?
-    ): CustomerPage {
-        return customerPageMapper(
-            fineractCNApiManager.getCustomerApi().fetchCustomers(pageIndex, size)
-        )
+    ): CustomerPageEntity {
+        return fineractCNApiManager.getCustomerApi().fetchCustomers(pageIndex, size)
     }
 
     override suspend fun updateCustomer(
         identifier: String,
-        customer: Customer
+        customer: CustomerEntity
     ): HttpResponse {
         return fineractCNApiManager.getCustomerApi()
-            .updateCustomer(identifier, customerEntityMapper(customer))
+            .updateCustomer(identifier, customer)
     }
 
     override suspend fun searchCustomer(
         pageIndex: Int?,
         size: Int?,
         term: String?
-    ): CustomerPage {
-        return customerPageMapper(
-            fineractCNApiManager.getCustomerApi().searchCustomer(pageIndex, size, term)
-        )
+    ): CustomerPageEntity {
+        return fineractCNApiManager.getCustomerApi().searchCustomer(pageIndex, size, term)
     }
 
     override suspend fun fetchCustomerDepositAccounts(
         customerIdentifier: String
-    ): List<DepositAccount> {
-        return depositAccountListMapper(
-            fineractCNApiManager.getDepositApi().fetchCustomerDeposits(customerIdentifier)
-        )
+    ): List<DepositAccountEntity> {
+        return fineractCNApiManager.getDepositApi().fetchCustomerDeposits(customerIdentifier)
     }
 
-    override suspend fun fetchDepositAccountDetails(accountIdentifier: String): DepositAccount {
-        return depositAccountMapper(
-            fineractCNApiManager.getDepositApi().fetchDepositAccountDetails(accountIdentifier)
-        )
+    override suspend fun fetchDepositAccountDetails(accountIdentifier: String): DepositAccountEntity {
+        return fineractCNApiManager.getDepositApi().fetchDepositAccountDetails(accountIdentifier)
     }
 
-    override suspend fun fetchProductDetails(productIdentifier: String): Product {
-        return productMapper(
-            fineractCNApiManager.getDepositApi().fetchProductDetails(productIdentifier)
-        )
+    override suspend fun fetchProductDetails(productIdentifier: String): ProductEntity {
+        return fineractCNApiManager.getDepositApi().fetchProductDetails(productIdentifier)
     }
 
     override suspend fun createDepositAccount(
-        depositAccountPayload: DepositAccountPayload
+        depositAccountPayload: DepositAccountPayloadEntity
     ): HttpResponse {
         return fineractCNApiManager.getDepositApi()
-            .createDepositAccount(depositAccountPayloadEntityMapper(depositAccountPayload))
+            .createDepositAccount(depositAccountPayload)
     }
 
     override suspend fun fetchJournalEntries(
         dateRange: String,
         accountIdentifier: String
-    ): List<JournalEntry> {
-        return journalEntryListMapper(
-            fineractCNApiManager.getAccountingApi()
-                .fetchJournalEntries(dateRange, accountIdentifier)
-        )
+    ): List<JournalEntryEntity> {
+        return fineractCNApiManager.getAccountingApi()
+            .fetchJournalEntries(dateRange, accountIdentifier)
     }
 
-    override suspend fun fetchJournalEntry(entryIdentifier: String): JournalEntry {
-        return journalEntryMapper(
-            fineractCNApiManager.getAccountingApi().fetchJournalEntry(entryIdentifier)
-        )
+    override suspend fun fetchJournalEntry(entryIdentifier: String): JournalEntryEntity {
+        return fineractCNApiManager.getAccountingApi().fetchJournalEntry(entryIdentifier)
     }
 }
